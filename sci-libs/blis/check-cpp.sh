53a97f5e221cac7792b15217a07a73bc36e7b99e
Mo Zhou <cdluminate@gmail.com>
2019-05-18 11:30:55 +0000

    check-cpp.sh

--- sci-libs/blis/check-cpp.sh
+++ sci-libs/blis/check-cpp.sh
@@ -4,3 +4,11 @@ export BLIS_NUM_THREADS=4
 
 LD_LIBRARY_PATH=/usr/lib64/blas/reference ./test-gemm
 LD_LIBRARY_PATH=/usr/lib64/blas/blis ./test-gemm
+
+eselect cblas set reference || true
+if ! test -r /usr/lib64/libcblas.so.3; then
+	ln -s libcblas.so.0 /usr/lib64/libcblas.so.3
+fi
+./test-gemm
+eselect cblas set blis || true
+./test-gemm
