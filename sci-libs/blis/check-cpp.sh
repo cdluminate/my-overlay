064ddfc3f1b2fc39efbac5a72fdf277fa047d213
Mo Zhou <cdluminate@gmail.com>
2019-05-23 02:09:14 +0000

    blis: fix minor issue in check-cpp.sh

--- sci-libs/blis/check-cpp.sh
+++ sci-libs/blis/check-cpp.sh
@@ -5,10 +5,9 @@ export BLIS_NUM_THREADS=4
 LD_LIBRARY_PATH=/usr/lib64/blas/reference ./test-gemm
 LD_LIBRARY_PATH=/usr/lib64/blas/blis ./test-gemm
 
+eselect blas set reference || true
 eselect cblas set reference || true
-if ! test -r /usr/lib64/libcblas.so.3; then
-	ln -s libcblas.so.0 /usr/lib64/libcblas.so.3
-fi
 ./test-gemm
+eselect blas set blis || true
 eselect cblas set blis || true
 ./test-gemm
