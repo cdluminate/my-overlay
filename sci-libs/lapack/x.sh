c27465d2bc20c502afd604d7bcaab7a6ace5f5ba
Mo Zhou <cdluminate@gmail.com>
2019-05-23 01:44:27 +0000

    lapack: use 3.8

--- sci-libs/lapack/x.sh
+++ sci-libs/lapack/x.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-ebuild lapack-3.7.0.ebuild digest
-ebuild lapack-3.7.0.ebuild clean
-ebuild lapack-3.7.0.ebuild install
+ebuild lapack-3.8.0.ebuild digest
+ebuild lapack-3.8.0.ebuild clean
+ebuild lapack-3.8.0.ebuild install
