ff2fb88106c9ec1af4fb9d330cdf85235d88419a
Mo Zhou <cdluminate@gmail.com>
2019-06-11 06:24:19 +0000

    overhaul blas.eselect and make it saner

---
+++ app-eselect/eselect-blas/files/install-eselect.sh
@@ -0,0 +1,2 @@
+cp -v blas.eselect-0.2 /usr/share/eselect/modules/blas.eselect
+#cp -v lapack.eselect /usr/share/eselect/modules/
--- sci-libs/lapack/files/install-eselect.sh
+++
@@ -1,2 +0,0 @@
-cp -v blas.eselect /usr/share/eselect/modules/
-cp -v lapack.eselect /usr/share/eselect/modules/
